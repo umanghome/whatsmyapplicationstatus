4cabeb3e5d55caaa894c22eb7ff9cb90d1c1e4a1
Umang Galaiya <umang.galaiya@razorpay.com>
2019-02-28 21:10:03 +0530

    Log in.

--- crawler.js
+++ crawler.js
@@ -1,7 +1,18 @@
 const login = async (browser, config) => {
+  const {
+    credentials,
+    pages,
+    selectors,
+  } = config;
+
   const page = await browser.newPage();
 
-  await page.goto(config.pages.login);
+  await page.goto(pages.login);
+
+  await page.type(selectors.login.username, credentials.username);
+  await page.type(selectors.login.password, credentials.password);
+  await page.click(selectors.login.submit);
+  await page.waitForNavigation();
 
   return page;
 }
